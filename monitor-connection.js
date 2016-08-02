810e94b0e7e41d9e59116ef4555e42d2b0aa564a
blcook223 <benjamin.l.cook@gmail.com>
2016-08-01 20:32:15 -0400

    added start time

--- monitor-connection.js
+++ monitor-connection.js
@@ -4,14 +4,16 @@ var ping = require('ping');
 
 var outage = false;
 
+fs.appendFile('./network.log', 'Monitoring started at ' + moment().format('MMMM Do YYYY, h:mm:ss a') + '\n');
+
 setInterval(function () {
     ping.sys.probe('8.8.8.8', function(isAlive){
         if (!outage && !isAlive) {
             outage = true;
-            fs.appendFile('./network.log', 'Network outage at ', moment().format('MMMM Do YYYY, h:mm:ss a'));
+            fs.appendFile('./network.log', 'Network outage at ' + moment().format('MMMM Do YYYY, h:mm:ss a') + '\n');
         } else if (outage && isAlive) {
             outage = false;
-            fs.appendFile('./network.log', 'Network restored at ', moment().format('MMMM Do YYYY, h:mm:ss a'));
+            fs.appendFile('./network.log', 'Network restored at ' + moment().format('MMMM Do YYYY, h:mm:ss a') + '\n');
         }
     });
 }, 5000);
